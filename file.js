a59a5db3c0d3eee76488a06b462459d897ce6ffc
Janek <gornada@gmail.com>
2025-10-09 20:01:23 +0200

    [1.12] funkcje - zadanie

--- file.js
+++ file.js
@@ -1,1 +1,28 @@
 console.log('Hello, World!');
+
+{
+    var test = "Test"
+    console.log(test);
+}
+console.log(test);
+
+let now = new Date();
+console.log(now);
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+let day = now.getDate();
+console.log(`Today is ${year}-${month}-${day}`);
+console.log(greet("John", "Doe"));
+
+function greet(name, surname) {
+    return `Hello, ${name} ${surname}!`;
+}
+arrowFunction = (name, surname) => {return `Hello, ${name} ${surname}!`;}
+
+console.log(arrowFunction("Jane", "Doe"));
+
+varFunction = function(name, surname) {
+    return `Hello, ${name} ${surname}!`;
+}
+
+console.log(varFunction("Jim", "Beam"));
