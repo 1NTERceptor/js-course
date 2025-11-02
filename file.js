35e538d09b49645d1e4529db84fa520b6130740d
Janek <gornada@gmail.com>
2025-11-02 13:56:37 +0100

    ćwiczenia ze stringów i interpolacji JS

--- file.js
+++ file.js
@@ -15,22 +15,30 @@ console.log(`Today is ${year}-${month}-${day}`);
 console.log(greet("John", "Doe"));
 
 function greet(name, surname) {
-    return `Hello, ${name} ${surname}!`;
+    return `Hello, ${name.charAt(0)} ${surname.charAt(0)}!`;
 }
-const arrowFunction = (name, surname) => {return `Hello, ${name} ${surname}!`;}
+const arrowFunction = (name, surname) => {return "Hello, ".concat(name, " ",surname, "!")}
 
 console.log(arrowFunction("Jane", "Doe"));
 
-const varFunction = function(name, surname) {
-    return `Hello, ${name} ${surname}!`;
+const varFunction = function(fullname) {
+    spacerIndex = fullname.indexOf(" ");
+    
+    return `Hello, ${fullname.charAt(0)} ${fullname.charAt(spacerIndex+1)}!`;
 }
 
-console.log(varFunction("Jim", "Beam"));
+console.log(varFunction("Jim Beam"));
 
 
 const numbers =(a, b) => {
     return a % b === 0;
 }
 
-console.log(numbers(10, 2));
+console.log(numbers(10, 2)); 
 console.log(numbers(10, 3));
+
+function generateInvoice(productName, quantity, unitPrice) {
+    console.log("Faktura: ".concat(quantity, " szt. ", productName, " - Razem do zapłaty: ", unitPrice*quantity, " zł."));
+}
+
+generateInvoice('Laptop', 2, 2500)
