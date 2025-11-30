07e8399fb74946a791c395ef73232fe734179815
Janek <gornada@gmail.com>
2025-11-30 14:01:10 +0100

    [2.22-2.23] OPERACJE NA tablicach

--- file.js
+++ file.js
@@ -42,3 +42,64 @@ function generateInvoice(productName, quantity, unitPrice) {
 }
 
 generateInvoice('Laptop', 2, 2500)
+
+let tabnumbers = [1,2,3]
+
+console.log(tabnumbers[0]);
+
+tabnumbers.push(47);
+console.log("after push",tabnumbers);
+
+const number = tabnumbers.pop();
+console.log("after pop",tabnumbers);
+console.log("popped number:", number);
+
+tabnumbers.unshift(0);
+console.log("after unshift",tabnumbers);
+
+const firstNumber = tabnumbers.shift();
+console.log("after shift",tabnumbers);
+console.log("shifted number:", firstNumber);
+
+tabnumbers.splice(1,2);
+console.log("after splice",tabnumbers);
+
+tabnumbers.splice(1,0,888,999)
+console.log("after splice insert",tabnumbers);
+
+const indexOf = tabnumbers.indexOf(999);
+tabnumbers.splice(indexOf,1);
+console.log("after removing 999",tabnumbers);
+
+filtereTable = tabnumbers.filter((num) => num > 50);
+console.log("after filter >50",filtereTable);
+
+const bignumber = tabnumbers.find((num) => num > 800);
+console.log("first number >800 : ",bignumber);
+
+const includesX = tabnumbers.includes(1);
+console.log("includes 1 :",includesX);
+
+console.log("all positive", tabnumbers.every((num) => num > 0));
+
+// odracanie tablicy
+
+tabnumbers.reverse();
+console.log("after reverse",tabnumbers);
+
+function swapArrayElements(arr, index1, index2) {
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+function countBiggerThan(arr, num) {
+    return arr.reduce((count, current) => current > num ? count + 1 : count, 0);
+}
+
+let sampleArray = [10, 20, 30, 40, 50];
+console.log("array beforswaping",sampleArray);
+swapArrayElements(sampleArray, 1, 3);
+console.log("after swapping index 1 and 3",sampleArray);
+let count = countBiggerThan(sampleArray, 25);
+console.log("elements bigger than 25",count);
